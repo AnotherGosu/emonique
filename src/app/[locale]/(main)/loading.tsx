442fcb93d546e1ea56a1a122446caf49677cd684
AnotherGosu <anothergosu@gmail.com>
2025-04-13 20:38:59 +0900

    Loading UI

--- src/app/[locale]/(main)/loading.tsx
+++ src/app/[locale]/(main)/loading.tsx
@@ -3,12 +3,12 @@ import { Loader } from "@/components/Status";
 
 export default function Loading() {
   return (
-    <>
+    <div className="grid h-full grid-rows-[max-content_1fr]">
       <HeroSection heading="Emonique" />
 
-      <div className="flex h-96 items-center justify-center">
+      <div className="flex items-center justify-center">
         <Loader />
       </div>
-    </>
+    </div>
   );
 }
