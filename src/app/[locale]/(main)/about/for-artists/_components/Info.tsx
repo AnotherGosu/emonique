1b055a0effb62e409f3c47ece230a18e52c387dd
AnotherGosu <anothergosu@gmail.com>
2025-04-18 23:53:51 +0900

    Image fixes

--- src/app/[locale]/(main)/about/for-artists/_components/Info.tsx
+++ src/app/[locale]/(main)/about/for-artists/_components/Info.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 import { PropsWithDict } from "@/types/common";
 
+import { IMAGE_PLACEHOLDER } from "@/constants/common";
+
 import banner from "@/images/about-us.png";
 
 import { cn } from "@/utils/cn";
@@ -23,8 +25,10 @@ export const Info = ({ dict }: PropsWithDict) => {
           src={banner}
           alt=""
           width={350}
-          className="rounded-xs"
+          height={465}
+          placeholder={IMAGE_PLACEHOLDER}
           priority
+          className="rounded-xs"
         />
       </div>
     </Section>
